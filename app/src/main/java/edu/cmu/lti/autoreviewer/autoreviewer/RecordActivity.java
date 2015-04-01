797b8a6339943d72cb763cabf4dcee80f606af35
Zhu Qiwen <qiwenzhu@ciilabs.org>
2015-04-01 17:33:17 -0400

    play video by clicking the picture

--- app/src/main/java/edu/cmu/lti/autoreviewer/autoreviewer/RecordActivity.java
+++ app/src/main/java/edu/cmu/lti/autoreviewer/autoreviewer/RecordActivity.java
@@ -3,6 +3,7 @@ package edu.cmu.lti.autoreviewer.autoreviewer;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -87,6 +88,16 @@ public class RecordActivity extends ActionBarActivity  {
         Thread newThread = new Thread(uploader);
         newThread.start();
 
+    }
+
+    public void goToSo (View view) {
+        goToUrl ( "https://www.youtube.com/watch?v=Kzt_j07-lkE&list=PLKMjqoaBW16XbjBbbdFmfbsYj5frmz6js");
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 
     @Override
